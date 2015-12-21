89deb1e19eb631525f6ad77f77cc559c1d2f94d5
Lossless <github@philkunz.com>
2015-12-21 02:54:45 +0000

    now parsing env information

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var c9workspace = {
     ideUrl: "undefined",
     serveUrl: "undefined"
 };
+(function () {
+    c9workspace.owner = process.env.C9_USER;
+    c9workspace.ideUrl = "https://ide.c9.io/" + process.env.C9_USER + "/" + c9workspace.name;
+    c9workspace.serveUrl = "https://" + c9workspace.name + "-" + c9workspace.owner + ".c9user.io";
+})();
 var c9config = {
     report: function () {
         var reportTable = plugins.beautylog.table.new("custom", ["c9 propertyName".cyan, "c9 value".cyan]);
@@ -15,6 +20,7 @@ var c9config = {
             reportTable.push([key, c9workspace[key]]);
         }
         reportTable.print();
-    }
+    },
+    workspace: c9workspace
 };
 module.exports = c9config;
--- ts/test.ts
+++ ts/test.ts
@@ -1,4 +1,3 @@
 /// <reference path="./typings/tsd.d.ts" />
 var c9config = require("./index.js");
-
 c9config.report();
